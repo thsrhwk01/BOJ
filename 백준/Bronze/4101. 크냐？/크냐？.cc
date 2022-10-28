58c251ddf68feaceb77d9fa89b3fc3de14b1d6b5
thsrhwk01 <114151294+thsrhwk01@users.noreply.github.com>
2022-10-28 10:23:43 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.cc
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -4,8 +4,7 @@ using namespace std;
 
 int main() {
     int i, j;
-    while (cin >> i >> j && (i != 0 && j != 0)) {
-        if (i > j) cout << "Yes\n";
-        else cout << "No\n";
-    }
+    
+    while (cin >> i >> j && (i != 0 && j != 0)) 
+        cout << (i > j ? "Yes" : "No") << '\n';
 }
