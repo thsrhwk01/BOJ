042e114f18ca6f030327e3431756f8846f875393
thsrhwk01 <114151294+thsrhwk01@users.noreply.github.com>
2022-11-09 13:19:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1884 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,22 +1,38 @@
-#include <iostream>
-#include <map>
-#include <algorithm>
-#include <iterator>
+//
+// Created by woo-young Choi on 2022/11/09.
+//
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
+#define endl '\n'
+#define pii pair<int, int>
+#define vii vector<pii>
+#define ll long long
+
+#define em emplace
+#define emb emplace_back
+#define range(x) x.begin(), x.end()
+#define fastio iostream::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+int n;
+vector<int> vec(10001);
+
+void input() {
+    cin >> n;
+    while (cin >> n) ++vec[n];
+}
+
+void output() {
+    for (int i = 1; i <= 10000; ++i)
+        for (int j = 0; j < vec[i]; ++j)
+            cout << i << endl;
+}
+
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int n; cin >> n;
+    fastio
 
-    map<int, int> mp;
-    while (n--) {
-        int tmp; cin >> tmp;
-        mp[tmp]++;
-    }
-
-    ostream_iterator<int> os(cout, "\n");
-    for (const auto& x : mp) {
-        fill_n(os, x.second, x.first);
-    }
+    input();
+    output();
 }
