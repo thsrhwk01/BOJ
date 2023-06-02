9ed888895f9be5669c607f6a185c0fea4040d0d8
thsrhwk01 <114151294+thsrhwk01@users.noreply.github.com>
2023-06-02 21:24:27 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 136 ms, Memory: 49872 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+
+#define endl '\n'
+#define em emplace
+#define emb emplace_back
+#define all(x) x.begin(), x.end()
+#define rep(i, from, to) for (int i = from; i <= to; ++i)
+#define fastio                                                                 \
+    iostream::sync_with_stdio(false);                                          \
+    cin.tie(nullptr);                                                          \
+    cout.tie(nullptr);
+
+bool board[7000][7000];
+
+int n;
+
+void input() { cin >> n; }
+
+void divideConquer(int y, int x, int length) {
+    if (length == 1) {
+        board[y][x] = true;
+        return;
+    }
+
+    int nextLength = length / 3;
+
+    rep(i, 0, 2) {
+        rep(j, 0, 2) {
+            if (i == 1 && j == 1)
+                continue;
+
+            divideConquer(y + i * nextLength, x + j * nextLength, nextLength);
+        }
+    }
+}
+
+void solve() { divideConquer(1, 1, n); }
+
+void output() {
+    rep(i, 1, n) {
+        rep(j, 1, n) cout << (board[i][j] ? '*' : ' ');
+        cout << endl;
+    }
+}
+
+int main() {
+    fastio;
+
+    input();
+    solve();
+    output();
+}
