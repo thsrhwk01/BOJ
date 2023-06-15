1c2a3b0b2b4275ecafb2d628dfc7b4e4b41a977f
thsrhwk01 <114151294+thsrhwk01@users.noreply.github.com>
2023-06-15 15:15:31 +0900

    [Gold IV] Title: 문자열 폭발, Time: 12 ms, Memory: 6072 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,92 +1,59 @@
-//
-// Created by woo-young Choi on 2022/10/31.
-//
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define endl '\n'
-#define pii pair<int, int>
-#define vii vector<pii>
-#define ll long long
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
 
+#define endl '\n'
 #define em emplace
 #define emb emplace_back
-#define range(x) x.begin(), x.end()
-#define fastio iostream::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define all(x) x.begin(), x.end()
+#define rep(i, from, to) for (int i = from; i <= to; ++i)
+#define fastio                                                                 \
+    iostream::sync_with_stdio(false);                                          \
+    cin.tie(nullptr);                                                          \
+    cout.tie(nullptr);
 
-vector<bool> checking;
-string s1, s2;
+stack<char> stk;
+string orig, bomb, ans;
 
-void input() {
-    cin >> s1 >> s2;
-    checking.resize(s1.size());
-    fill(checking.begin(), checking.end(), true);
-}
-
-bool eraser(int &i) {
-    int start = i;
-    auto now2 = s2.begin() + 1;
-
-    ++i;
-    bool checker = [&] {
-        vii ranges;
-
-        while (now2 != s2.end()) {
-            if (i >= s1.size()) return false;
-            if (s1[i] == s2.front()) {
-                ranges.emb(start, i);
-                checker = eraser(i);
-                start = i;
-                if (!checker) return false;
-            } else if (s1[i] == *now2) {
-                ++i;
-                ++now2;
-                continue;
-            } else return false;
-        }
-
-        for (auto x: ranges) {
-            fill(checking.begin() + x.first, checking.begin() + x.second, false);
-        }
-        fill(checking.begin() + start, checking.begin() + i, false);
-
-        return true;
-    }();
-
-    return checker;
-}
+void input() { cin >> orig >> bomb; }
 
 void solve() {
-    int i = 0;
+    rep(i, 0, orig.size() - 1) {
+        ans += orig[i];
 
-    while (i < s1.size()) {
-        if (s1[i] == s2.front()) {
-            eraser(i);
+        if (ans.back() == bomb.back()) {
+
+            [&] {
+                while (true) {
+                    if (ans.size() < bomb.size())
+                        return;
+
+                    int j = ans.size() - 1;
+                    int k = bomb.size() - 1;
+
+                    while (k >= 0)
+                        if (bomb[k--] != ans[j--])
+                            return;
+
+                    k = bomb.size();
+                    while (k--)
+                        ans.pop_back();
+                }
+            }();
         }
-        else ++i;
     }
 }
 
-void output() {
-    int i = 0;
-    [&] {
-        while (i < s1.size()) {
-            if (checking[i]) return;
-            ++i;
-        }
-        cout << "FRULA";
-    }();
-
-    while (i < s1.size()) {
-        if (checking[i]) cout << s1[i];
-        ++i;
-    }
-}
+void output() { cout << (ans.empty() ? "FRULA" : ans); }
 
 int main() {
-    fastio
+    fastio;
 
     input();
     solve();
