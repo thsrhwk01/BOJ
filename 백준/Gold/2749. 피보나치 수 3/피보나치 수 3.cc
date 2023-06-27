42a43e9d4462f7e5de3730d80ae176d6faa883e9
thsrhwk01 <114151294+thsrhwk01@users.noreply.github.com>
2023-06-28 01:08:14 +0900

    [Gold II] Title: 피보나치 수 3, Time: 12 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+
+#define endl '\n'
+#define em emplace
+#define emb emplace_back
+#define all(x) x.begin(), x.end()
+#define rep(i, from, to) for (int i = from; i <= to; ++i)
+#define fastio                                                                 \
+    iostream::sync_with_stdio(false);                                          \
+    cin.tie(nullptr);                                                          \
+    cout.tie(nullptr);
+
+ll fibonacci[1'500'050], n;
+
+void input() { cin >> n; }
+
+void solve() {
+    fibonacci[1] = 1;
+    fibonacci[2] = 1;
+
+    rep(i, 1, 1'500'010) {
+        fibonacci[i + 2] = (fibonacci[i + 1] + fibonacci[i]) % 1'000'000;
+    }
+}
+
+void output() { cout << fibonacci[n % 1500000]; }
+
+int main() {
+    fastio;
+
+    input();
+    solve();
+    output();
+}
