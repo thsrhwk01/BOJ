b3d77dedfdba8e2d8b49fe3a65e9425304d476b6
thsrhwk01 <114151294+thsrhwk01@users.noreply.github.com>
2023-07-13 17:20:44 +0900

    [Gold V] Title: 자릿수, Time: 0 ms, Memory: 2116 KB -BaekjoonHub

---
+++ 백준/Gold/28293. 자릿수/자릿수.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+
+#define endl '\n'
+#define em emplace
+#define emb emplace_back
+#define all(x) x.begin(), x.end()
+#define rep(i, from, to) for (int i = from; i <= to; ++i)
+#define fastio                                                                 \
+    iostream::sync_with_stdio(false);                                          \
+    cin.tie(nullptr);                                                          \
+    cout.tie(nullptr);
+
+int a, b;
+
+ll res;
+
+void input() { cin >> a >> b; }
+
+void solve() { res = b * (log10(a)) + 1; }
+
+void output() { cout << res; }
+
+int main() {
+    fastio;
+
+    input();
+    solve();
+    output();
+}
