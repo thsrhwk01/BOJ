b6bdfa6ebf349967ccbd66b4bb201fb7bd280b52
thsrhwk01 <114151294+thsrhwk01@users.noreply.github.com>
2024-11-06 21:24:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 116 ms, Memory: 4380 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,73 +1,54 @@
-//
-// Created by woo-young Choi on 2022/11/09.
-//
-
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <numeric>
 
 using namespace std;
 
-#define endl '\n'
-#define pii pair<int, int>
-#define vii vector<pii>
-#define ll long long
+vector<int> parent;
 
-#define em emplace
-#define emb emplace_back
-#define range(x) x.begin(), x.end()
-#define fastio iostream::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-
-vector<pair<int, pii>> edges;
-vector<int> union_find;
-int node_num, edge_num;
-ll ans = 0;
-
-int getParent(int x) {
-    return union_find[x] == x ? x : getParent(union_find[x]);
+int findSet(int x) {
+    return x == parent[x] ? x : parent[x] = findSet(parent[x]);
 }
 
-bool isSameParent(int x, int y) {
-    return getParent(x) == getParent(y);
+void unionSet(int x, int y) {
+    x = findSet(x);
+    y = findSet(y);
+
+    parent[max(x, y)] = min(x, y);
 }
 
-void unionParent(int x, int y) {
-    x = getParent(x);
-    y = getParent(y);
-
-    union_find[max(x, y)] = min(x, y);
-}
-
-void input() {
-    cin >> node_num >> edge_num;
-    union_find.resize(node_num + 1);
-    iota(range(union_find), 0);
-
-    edges.resize(edge_num);
-
-    for (auto &edge: edges) {
-        cin >> edge.second.first >> edge.second.second >> edge.first;
-    }
-}
-
-void solve() {
-    sort(range(edges));
-
-    for (const auto &edge: edges) {
-        int s = edge.second.first, e = edge.second.second;
-        if (isSameParent(s, e)) continue;
-
-        unionParent(s, e);
-        ans += edge.first;
-    }
-}
-
-void output() {
-    cout << ans;
-}
+bool isConnected(int x, int y) { return findSet(x) == findSet(y); }
 
 int main() {
-    fastio
+    int v, e;
+    cin >> v >> e;
 
-    input();
-    solve();
-    output();
+    parent.resize(v + 5);
+
+    iota(parent.begin(), parent.end(), 0);
+
+    vector<tuple<int, int, int>> edges;
+    while (e--) {
+        int from, to, weight;
+        cin >> from >> to >> weight;
+
+        edges.emplace_back(weight, from, to);
+    }
+
+    sort(edges.begin(), edges.end());
+
+    long long ans = 0;
+
+    for (const auto &[weight, from, to] : edges) {
+        if (isConnected(from, to)) {
+            continue;
+        }
+
+        ans += weight;
+
+        unionSet(from, to);
+    }
+
+    cout << ans;
 }
